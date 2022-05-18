593eb3f441d053c8b99f0829072895ccdbe8dd5a
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2022-05-18 14:26:35 -0400

    seeds ready

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,30 @@
 INSERT INTO departments (name)
 
 VALUES
+    ('executive'),
     ('accounting'),
     ('shipping'),
     ('sales');
+
+
+INSERT INTO roles (jobtitle, department, salary)
+
+VALUES
+    ('owner', 'executive', '$120,000'),
+    ('manager', 'accounting', '$90,000'),
+    ('manager', 'shipping', '$80,000'),
+    ('manager', 'sales', '$110,000'),
+    ('accountant', 'accounting', '$75,000'),
+    ('clerk', 'shipping', '$65,000'),
+    ('associate', 'sales', '$80,000');
+
+INSERT INTO employees (firstname, lastname, jobtitle, department, salary, manager)
+
+VALUES
+    ('homer', 'simpson', 'owner', 'executive', '$120,000', 'n/a'),
+    ('bart', 'simpson', 'manager', 'shipping', '$80,000', 'homer simpson'),
+    ('lisa', 'simpson', 'manager', 'accounting', '$90,000', 'homer simpson'),
+    ('marge', 'simpson', 'manager', 'sales', '$110,000', 'homer simpson'),
+    ('barney', 'gumbal', 'clerk', 'shipping', '$65,000', 'bart simpson'),
+    ('moe', 'szyzlak', 'accountant', 'accounting', '$75,000', 'lisa simpson'),
+    ('seymour', 'skinner', 'associate', 'sales', '$80,000', 'marge simpson');
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+
 // Connect to database
 const db = mysql.createConnection(
     {
@@ -11,4 +15,64 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the mybusiness database.')
   );
-  
+
+function firstPrompt(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'userAction',
+                message: 'What do you want to do next?',
+                choices: [
+                    'view all departments', 'view all roles', 'view all employees', new inquirer.Separator(), 'add a department', 'add a role', 'add an employee', 'update an employee role'
+                ],
+            }
+        ])
+        
+        .then((answers) => {
+            console.log(answers.userAction)
+            switch(answers.userAction){
+                case 'view all departments':
+                    db.query(`SELECT * FROM departments`, (err, rows) => {
+                        console.table(rows);
+                        firstPrompt();
+                    });
+                    
+                break
+                case 'view all roles':
+                    db.query(`SELECT * FROM roles`, (err, rows) => {
+                        console.table(rows);
+                        firstPrompt();
+                    });
+                break
+                case 'view all employees':
+                    db.query(`SELECT * FROM employees`, (err, rows) => {
+                        console.table(rows);
+                        firstPrompt();
+                    });
+                break
+                case 'add a department':
+                    
+                break
+                case 'add a role':
+                    
+                break
+                case 'add an employee':
+        
+                break
+                case 'update an employee role':
+        
+                break
+
+            }
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            } else {
+            // Something else went wrong
+            }
+    });
+}
+
+firstPrompt();
